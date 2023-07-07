83eca0be051bff8a0b095cb5424a77f504db93f9
DVDFE <davidfe.pro@gmail.com>
2023-07-07 16:53:04 +0200

    ajout de components + props

---
+++ src/components/CareScale.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CareScale = ({ scaleValue, careType }) => {
+
+    const range = [1, 2, 3]
+	const scaleType = careType === 'light' ? '☀️' : '💧'
+
+    return (
+        <div>
+            {range.map((rangeElem) =>
+				scaleValue >= rangeElem ? (
+					<span key={rangeElem.toString()}>{scaleType}</span>
+				) : null
+			)}
+        </div>
+    );
+};
+
+export default CareScale;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { plantList } from "../datas/plantList";
+import CareScale from "./CareScale";
+import PlantItem from "./PlantItem";
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -9,22 +11,24 @@ function ShoppingList() {
 	)
 
 	return (
-		<div>
+<div>
 			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
-                        {plant.isSpecialOffer && <div className="lmj-sales">Solde</div> }
-					</li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
 	)
 }
-
 export default ShoppingList
