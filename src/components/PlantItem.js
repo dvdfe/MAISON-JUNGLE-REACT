7c4e94cd202871993aa647ae7967c5783614ecb5
DVDFE <davidfe.pro@gmail.com>
2023-07-10 14:51:29 +0200

    ajout datas+css+onclick

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -2,8 +2,12 @@ import React from 'react';
 import CareScale from './CareScale';
 
 const PlantItem = ({ id, cover, name, water, light }) => {
+
+    const handleClick = (name)=>{
+        alert(`hello ! ici se trouve la ${name}`)
+    }
     return (
-        <li key={id} className='lmj-plant-item'>
+        <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
